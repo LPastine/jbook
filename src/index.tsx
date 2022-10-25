8131d290fe26af1dc8a82999db5b4f141b257846
LPastine <lucianopastine.tech@gmail.com>
2022-10-25 17:17:16 -0300

    code cell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
-import { useState } from 'react';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState<string>('');
-
-  const onClick = async () => {
-    const output: string = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
