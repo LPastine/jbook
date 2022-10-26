7390b90de6208419b648f5a8bc545491b1cf255b
LPastine <lucianopastine.tech@gmail.com>
2022-10-26 18:10:17 -0300

    testing the rendering of the cells list

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,9 +1,18 @@
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
+
 interface CellListItemProps {
   cell: Cell;
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
-  return <div>{cell.id}</div>;
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+  return <>{child}</>;
 };
 
 export default CellListItem;
